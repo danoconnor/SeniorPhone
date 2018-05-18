f74bf05e28b66263fb7b914325020f26aef18f4a
Dan O'Connor <dpo3ys@virginia.edu>
2018-05-17 20:04:45 -0400

    Very rough home screen prototype

--- src/app/src/main/java/com/docproductions/seniorphone/HomeScreenActivity.java
+++ src/app/src/main/java/com/docproductions/seniorphone/HomeScreenActivity.java
@@ -2,6 +2,12 @@ package com.docproductions.seniorphone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.docproductions.seniorphone.Adapters.MainAppAdapter;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -9,5 +15,13 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        GridView mainAppsView = findViewById(R.id.mainAppsView);
+        mainAppsView.setAdapter(new MainAppAdapter(this));
+        mainAppsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(HomeScreenActivity.this, "Hello world: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
